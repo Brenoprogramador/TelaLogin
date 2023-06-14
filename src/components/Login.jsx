c56a09db040726c0c05b4ef571263f33a81067be
breno nunes souza <brenoprogramador1928@gmail.com>
2023-06-13 21:42:43 -0300

    Trabalho concluido com todas as suas funções

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,34 @@
 import { useState } from 'react'
 import logo from '../img/logo.png'
 import './Login.css'
+import showPasswordIcon from '../img/eye.svg'
+import hidePasswordIcon from '../img/eye-off.svg'
 
 export function Login() {
     const [senha, setSenha] = useState("")
     const [email, setEmail] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
 
-    function handleLogin(){
-        return alert(`Seu email é: ${email}\nSua senha é: ${senha}`)
-    }
-
-    function mostrarSenha(){
-        let senhaInput = document.getElementById("Senha")
-        if (senhaInput.type === "password"){
-            senhaInput.type = 'text'
-        }else{
-            senhaInput.type = 'password'
+    function handleLogin() {
+        if (validateEmail(email)) {
+            return alert(`Seu email é: ${email}\nSua senha é: ${senha}`)
+        } else {
+            setEmailError("Digite um email válido")
         }
     }
-    return(
 
+    function validateEmail(email) {
+        // Implemente aqui sua lógica de validação de email
+        // Neste exemplo, apenas verifica se o email possui um @
+        return email.includes("@")
+    }
+
+    function toggleShowPassword() {
+        setShowPassword(!showPassword)
+    }
+
+    return (
         <main>
             <section className='login'>
                 <div className="content">
@@ -32,16 +41,38 @@ export function Login() {
                     <p>Faça login ou registre-se para começar a construir seus projetos ainda hoje.</p>
                     <form>
                         <div className="inputEmail">
-                            <label for="Email">E-mail</label>
-                            <input type="email" placeholder='Digite seu email' id="email" name='email' required
-                            onChange={(e) => setEmail(e.target.value)}/>
+                            <label htmlFor="email">E-mail</label>
+                            <input
+                                type="email"
+                                placeholder='Digite seu email'
+                                id="email"
+                                name='email'
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            {emailError && <p className="error">{emailError}</p>}
                         </div>
 
                         <div className="inputSenha">
-                            <label for="Senha">Senha</label>
-                            <input  type="password" placeholder='Digite sua senha' id="Senha" className='input__box'
-                            onChange={(e) => setSenha(e.target.value)}
-                            onClick={mostrarSenha}/>
+                            <div className="box">
+                            <label htmlFor="senha">Senha</label>
+                            <span className='esqueceuSenha'>Esqueceu a senha?</span>
+                            </div>
+                            <div className="passwordContainer">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder='Digite sua senha'
+                                    id="senha"
+                                    className='input__box'
+                                    onChange={(e) => setSenha(e.target.value)}
+                                />
+                                <img
+                                    className="passwordIcon"
+                                    src={showPassword ? hidePasswordIcon : showPasswordIcon}
+                                    alt={showPassword ? "Ocultar senha" : "Mostrar senha"}
+                                    onClick={toggleShowPassword}
+                                />
+                            </div>
                         </div>
 
                         <button className='buttonEntrar' onClick={handleLogin}>
@@ -51,15 +82,11 @@ export function Login() {
 
                     <p>Ainda não tem uma conta?<span className='destaque'> Inscreva-se</span></p>
                 </div>
-
             </section>
 
             <section className='capa'>
-                <div className='imagem'>'</div>
+                <div className='imagem'></div>
             </section>
-
         </main>
-        
     )
 }
-
